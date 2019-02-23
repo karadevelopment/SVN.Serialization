832782ade2323d9221cbc9da56881b2d1f8248fa
Muhammed Kara <47905966+svndevelopment@users.noreply.github.com>
2019-02-23 01:57:01 +0100

    1.0.0.0

---
+++ Compression/Extensions.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace SVN.Serialization.Compression
+{
+    public static class Extensions
+    {
+        public static byte[] GZipCompress(this byte[] input)
+        {
+            using (var inputStream = new MemoryStream(input))
+            {
+                using (var outputStream = new MemoryStream())
+                {
+                    using (var zipStream = new GZipStream(outputStream, CompressionMode.Compress))
+                    {
+                        inputStream.CopyTo(zipStream);
+                    }
+                    return outputStream.ToArray();
+                }
+            }
+        }
+
+        public static byte[] GZipDecompress(this byte[] input)
+        {
+            using (var inputStream = new MemoryStream(input))
+            {
+                using (var outputStream = new MemoryStream())
+                {
+                    using (var zipStream = new GZipStream(inputStream, CompressionMode.Decompress))
+                    {
+                        zipStream.CopyTo(outputStream);
+                    }
+                    return outputStream.ToArray();
+                }
+            }
+        }
+
+        public static void AddFilesToZip(this FileStream stream, params (string name, byte[] data)[] files)
+        {
+            using (var zipStream = new ZipArchive(stream, ZipArchiveMode.Update))
+            {
+                foreach (var (name, data) in files)
+                {
+                    var entry = zipStream.GetEntry(name) ?? zipStream.CreateEntry(name);
+
+                    using (var entryStream = entry.Open())
+                    {
+                        entryStream.Write(data, default(int), data.Length);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Protobuf/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using ProtoBuf;
+using System.IO;
+
+namespace SVN.Serialization.Protobuf
+{
+    public static class Extensions
+    {
+        public static byte[] SerializeProtobuf<T>(this T instance)
+        {
+            using (var stream = new MemoryStream())
+            {
+                Serializer.Serialize(stream, instance);
+                return stream.ToArray();
+            }
+        }
+
+        public static T DeserializeProtobuf<T>(this byte[] input)
+        {
+            using (var stream = new MemoryStream(input))
+            {
+                return Serializer.Deserialize<T>(stream);
+            }
+        }
+    }
+}
